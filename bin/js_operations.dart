e3edf0f26d47a87601cb96da7af276d9c9258164
mkhtradm01 <mkhtradm01@gmail.com>
2021-05-10 22:04:00 +0400

    first commit

---
+++ bin/js_operations.dart
@@ -0,0 +1,5 @@
+import 'package:js_operations/js_operations.dart' as js_operations;
+
+void main(List<String> arguments) {
+  print('Hello world: ${js_operations.calculate()}!');
+}
---
+++ test/js_operations_test.dart
@@ -0,0 +1,8 @@
+import 'package:js_operations/js_operations.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
