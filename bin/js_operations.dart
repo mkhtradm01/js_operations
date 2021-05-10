e34b2de3d59a1b9493ea374c54e2691268c85991
mkhtradm01 <mkhtradm01@gmail.com>
2021-05-11 00:18:26 +0400

    testing javascript

--- bin/js_operations.dart
+++ bin/js_operations.dart
@@ -2,4 +2,5 @@ import 'package:js_operations/js_operations.dart' as js_operations;
 
 void main(List<String> arguments) {
   print('Hello world: ${js_operations.calculate()}!');
+  print('${js_operations.greetings('Adamu')}!');
 }
--- lib/js_operations.dart
+++ lib/js_operations.dart
@@ -1,3 +1,8 @@
 int calculate() {
-  return 6 * 7;
+  var a = 10;
+  var b = 11.3;
+  var total = (a + b)~/2;
+  return total;
 }
+
+String greetings(String? name) => 'Hello $name! Good evening' ;
